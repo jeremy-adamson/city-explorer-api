7e842840158fcc4d5306f0e2acaa1084d780b6c6
jeremy-adamson <jadamson.physics@gmail.com>
2023-02-23 20:34:04 -0800

    movies key

--- movies.js
+++ movies.js
@@ -3,24 +3,24 @@
 const axios = require('axios');
 const cache = require('./cache');
 
-function getMovies(req, res, next){
-    const url = ``;
+function getMovies(req, res, next) {
+    const url = `https://api.themoviedb.org/3/movie/550?api_key=${MOVIE_API_KEY}`;
 
     const key = 'movies ' + city;
-    if(cache[key] && (Date.now() - cache[key].timestamp) < 1800000){
+    if (cache[key] && (Date.now() - cache[key].timestamp) < 1800000) {
         res.status(200).send(cache[key].data);
     }
     else {
-    axios.get(url)
-    .then(data => {
-        let formattedMovieData = data.map(element => new MovieInfo(element));
-        cache[key] = {};
-        cache[key].timestamp = Date.now();
-        cache[key].data = formattedMovieData;
-        res.status(200).send(formattedMovieData);
-    })
-    .catch(error => next(error));
-}
+        axios.get(url)
+            .then(data => {
+                let formattedMovieData = data.map(element => new MovieInfo(element));
+                cache[key] = {};
+                cache[key].timestamp = Date.now();
+                cache[key].data = formattedMovieData;
+                res.status(200).send(formattedMovieData);
+            })
+            .catch(error => next(error));
+    }
 }
 
 class MovieInfo {
